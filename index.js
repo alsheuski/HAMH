aadd9d6f993017f62d4dc67fef4b7a52c4543581
Alex Alsheuski <aleksey.alshevskiy@intellectsoft.net>
2018-06-08 12:33:18 +0300

    add processor and reportBuilder modules

--- index.js
+++ index.js
@@ -3,63 +3,35 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
-const files = require('./lib/files');
 const inquirer = require('./lib/inquirer');
-const parser = require('./lib/parser');
-const printer = require('./lib/printer/printer');
-const printerTargets = require('./lib/printer/targets');
+const processor = require('./lib/processors/processor');
 
-clear();
+const beforeRun = () => {
+    clear();
 
-console.log(
-    chalk.yellow(
-        figlet.textSync('HAMH', { horizontalLayout: 'full' })
-    )
-);
-console.log(
-    chalk.blue(
-        'HTMLAcademy Mentor\'s Helper CLI v.0.0.1'
-    )
-);
-console.log('');
-
-const processFile = (filename) => {
-    if (!files.fileExists(filename)) {
-        console.log(chalk.red(`-> ${filename} doesn\'t exists!`));
-    }
-
-    const fileContent = files.fileRead(filename);
-
-    if (!fileContent) {
-        console.log(chalk.red(`-> Error with reading ${filename}`));
-        return;
-    }
-
-    const printerTargetConsole = {
-        type: printerTargets.CONSOLE
-    };
-
-    const printerTargetFile = {
-        type: printerTargets.FILE,
-        filename: `report_${filename}`
-    };
-
-    const links = parser.getAllLinkElements(fileContent);
-    printer.print(links, [printerTargetConsole, printerTargetFile]);
-
-    const buttons = parser.getAllButtonsElements(fileContent);
-    printer.print(buttons, [printerTargetConsole]);
+    console.log(
+        chalk.yellow(
+            figlet.textSync('HAMH', { horizontalLayout: 'full' })
+        )
+    );
+    console.log(
+        chalk.blue(
+            'HTMLAcademy Mentor\'s Helper CLI v.0.0.1'
+        )
+    );
+    console.log('');
 };
+
 
 const run = async () => {
     const filesToCheck = (await inquirer.askFilesToCheck()).files;
 
     for (let i = 0; i < filesToCheck.length; i += 1) {
-        processFile(filesToCheck[i]);
+        processor.processFile(filesToCheck[i]);
     }
 };
 
-
+beforeRun();
 run();
 
 
