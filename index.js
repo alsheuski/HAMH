67fcadf973f94f2557390d7433d02916ac0f22a3
Alex Alsheuski <aleksey.alshevskiy@intellectsoft.net>
2018-06-14 10:59:38 +0300

    HTML validator fix, create templates for criterias

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const beforeRun = () => {
     );
     console.log(
         chalk.blue(
-            'HTMLAcademy Mentor\'s Helper CLI v.0.4.0'
+            'HTMLAcademy Mentor\'s Helper CLI v.0.4.1'
         )
     );
     console.log('');
