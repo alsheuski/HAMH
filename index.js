485309b35de525d1560de1d03fc2ab9e5f7c9b5c
Alex Alsheuski <aleksey.alshevskiy@intellectsoft.net>
2018-06-15 17:56:02 +0300

    Add b1 processor

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const beforeRun = () => {
     );
     console.log(
         chalk.blue(
-            'HTMLAcademy Mentor\'s Helper CLI v.0.5.0'
+            'HTMLAcademy Mentor\'s Helper CLI v.0.6.0'
         )
     );
     console.log('');
